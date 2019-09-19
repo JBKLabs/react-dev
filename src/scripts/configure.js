21fa0ab916bd4c878c2d0ae7d6956bc22b280ff6
Brian Maule <brian.maule@jbknowledge.com>
2019-09-19 12:21:01 -0500

    fix configure

--- src/scripts/configure.js
+++ src/scripts/configure.js
@@ -3,15 +3,18 @@ const shell = require('shelljs');
 
 const { appDirectory } = require('../util');
 
+shell.set('+v');
 Object.entries(process.env)
   .filter(([key]) => key.substr(0, 4) === 'ENV_')
   .forEach(kvp => {
     const key = kvp[0].substr(4);
     const value = kvp[1];
+
     shell.sed(
       '-i',
-      new RegExp(`${key}: [^,]+`),
+      new RegExp(`${key}: [^,{}]+`),
       `${key}: '${value}'`,
       path.join(appDirectory, 'dist/env.js')
     );
   });
+shell.set('-v');
