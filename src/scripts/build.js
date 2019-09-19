1f48fb0bff29a297bcdd312414f3354ee234d4c1
Brian Maule <brian.maule@jbknowledge.com>
2019-09-19 11:54:19 -0500

    copy public contents, not public itself

--- src/scripts/build.js
+++ src/scripts/build.js
@@ -8,7 +8,7 @@ const { appDirectory } = require('../util');
 const config = webpackFactory('production');
 
 shell.rm('-rf', path.join(appDirectory, 'dist/*'));
-shell.cp('-rf', path.join(appDirectory, 'public'), path.join(appDirectory, 'dist'));
+shell.cp('-rf', path.join(appDirectory, 'public/*'), path.join(appDirectory, 'dist'));
 
 const compiler = webpack(config);
 compiler.run((err, stats) => {
