bcbc0616f9e13847ea0d8c4b686b881f0a7e08e5
Brian Maule <brian.maule@jbknowledge.com>
2019-09-19 12:14:51 -0500

    define configure script

---
+++ src/scripts/configure.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const shell = require('shelljs');
+
+const { appDirectory } = require('../util');
+
+Object.entries(process.env)
+  .filter(([key]) => key.substr(0, 4) === 'ENV_')
+  .forEach(kvp => {
+    const key = kvp[0].substr(4);
+    const value = kvp[1];
+    shell.sed(
+      '-i',
+      new RegExp(`${key}: [^,]+`),
+      `${key}: '${value}'`,
+      path.join(appDirectory, 'dist/env.js')
+    );
+  });
