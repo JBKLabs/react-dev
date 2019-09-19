af01bb6cc78f47f65b852f410949ac4220ad0f5d
Josh Hooton <josh.hooton@jbknowledge.com>
2019-09-19 13:38:34 -0500

    Work towards setting default args.

--- src/scripts/lint.js
+++ src/scripts/lint.js
@@ -1,8 +1,24 @@
 const spawn = require('cross-spawn');
+const yargsParser = require('yargs-parser');
 
 const { resolveBin } = require('../util');
 
-const args = process.argv.slice(2);
+let args = process.argv.slice(2);
+console.log(process.argv);
+const parsedArgs = yargsParser(args);
+
+const filesGiven = parsedArgs._.length > 0;
+const filesToApply = filesGiven ? [] : ['.'];
+if (filesGiven) {
+  // we need to take all the flag-less arguments (the files that should be linted)
+  // and filter out the ones that aren't js files. Otherwise json or css files
+  // may be passed through
+  args = args.filter(
+    (arg) => !parsedArgs._.includes(arg) || /\.jsx?$/.test(arg)
+  );
+}
+
+console.log({ parsedArgs });
 
 const result = spawn.sync(resolveBin('eslint'), [...args], {
   stdio: 'inherit'
