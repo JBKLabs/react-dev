e9269acda90d9781b9a7f213edc0c2d9e2296d4f
Brian Maule <12001504+mauleb@users.noreply.github.com>
2019-09-19 11:45:43 -0500

    use shelljs instead of fs to clear dist

--- src/scripts/build.js
+++ src/scripts/build.js
@@ -1,4 +1,3 @@
-const fs = require('fs');
 const path = require('path');
 const shell = require('shelljs');
 const webpack = require('webpack');
@@ -8,7 +7,7 @@ const { appDirectory } = require('../util');
 
 const config = webpackFactory('production');
 
-fs.emptyDirSync(path.join(appDirectory, 'dist'));
+shell.rm('-rf', path.join(appDirectory, 'dist/*'));
 shell.cp('-rf', path.join(appDirectory, 'public'), path.join(appDirectory, 'dist'));
 
 const compiler = webpack(config);
