343c0220f47b70e0f9c617799edb71bb677e8351
Brian Maule <12001504+mauleb@users.noreply.github.com>
2019-09-20 15:33:17 -0500

    tweak location of default config exports

--- src/config/babel.js
+++ src/config/babel.js
@@ -1,41 +1,11 @@
-const unformattedPlugins = {
-  'babel-plugin-module-resolver': {
-    path: require.resolve('babel-plugin-module-resolver'),
-    root: ['.'],
-    alias: {
-      '~': '.',
-      src: './src'
-    }
-  }
-}
-
-const defaultConfig = {
-  presets: [
-    require.resolve('@babel/preset-env'),
-    require.resolve('@babel/preset-react')
-  ],
-  plugins: {
-    'babel-plugin-module-resolver': {
-      formatted: [
-        unformattedPlugins['babel-plugin-module-resolver'].path,
-        {
-          root: unformattedPlugins['babel-plugin-module-resolver'].root,
-          alias: unformattedPlugins['babel-plugin-module-resolver'].alias
-        }
-      ],
-      opt: unformattedPlugins['babel-plugin-module-resolver']
-    }
-  }
-};
-
-module.exports.defaultConfig = defaultConfig;
+const defaults = require('./defaults');
 
 module.exports = (api) => {
   api.cache(true);
   return {
-    presets: defaultConfig.presets,
+    presets: defaults.babel.presets,
     plugins: Object
-      .values(defaultConfig.plugins)
+      .values(defaults.babel.plugins)
       .map(p => p.formatted),
   }
 };
