569c3c5398c0e16e67bce8c9a4b6474a51a7abdd
Brian Maule <brian.maule@jbknowledge.com>
2019-09-23 13:47:54 -0500

    add prettier config defaults/replacement/lint

---
+++ src/scripts/lint/prettier.js
@@ -0,0 +1,53 @@
+const spawn = require('cross-spawn');
+const chalk = require('chalk');
+const path = require('path');
+const parse = require('yargs-parser');
+const unparse = require('yargs-unparser');
+
+const fetchConfig = require('../../util/fetchConfig');
+const { resolveBin, log } = require('../../util');
+
+let args = process.argv.slice(2);
+const parsedArgs = parse(args);
+
+const prettier = fetchConfig.prettier();
+let config = [
+  '--config',
+  path.join(__dirname, '../config/prettier.yml')
+];
+
+if (parsedArgs.prettierConfig) {
+  config = [
+    '--config',
+    path.join(appDirectory, parsedArgs.prettierConfig)
+  ];
+
+  delete parsedArgs['prettier-config'];
+  delete parsedArgs.prettierConfig;
+  log(`Using provided prettier config path: ${config[1]}`);
+} else if (prettier.configurationExists) {
+  config = prettier.path
+    ? [
+      '--config',
+      path.join(appDirectory, prettier.path)
+    ] : [];
+  log('Prettier override detected: ' + chalk.cyan(prettier.token));
+}
+
+const output = parsedArgs.c || parsedArgs.check || parsedArgs.l || parsedArgs.listDifferent || parsedArgs.write
+  ? []
+  : ['--check'];
+
+const filesGiven = parsedArgs._.length > 0;
+const filesToApply = filesGiven ? [] : ['src/**/*.js'];
+if (filesGiven) {
+  args = args.filter(
+    (arg) => !parsedArgs._.includes(arg) || /\.jsx?$/.test(arg)
+  );
+}
+
+const result = spawn.sync(resolveBin('prettier'), [...unparse(parsedArgs), ...config, ...output, ...filesToApply], {
+  stdio: 'inherit'
+});
+
+process.exit(result.status);
--- src/util/fetchConfig.js
+++ src/util/fetchConfig.js
@@ -11,14 +11,9 @@ const { package: pkg } = readPkgUp.sync({
 const buildPath = (...p) => path.join(appDirectory, ...p);
 const projectHasFile = (...p) => fs.existsSync(buildPath(...p));
 
-const babel = () => {
+const fetch = (files, pkgKey) => {
   let result = null;
-
-  [
-    '.babelrc',
-    '.babelrc.js',
-    '.babel.config.js'
-  ].forEach(token => {
+  files.reverse().forEach(token => {
     if (projectHasFile(token)) {
       result = {
         token,
@@ -26,38 +21,39 @@ const babel = () => {
         path: buildPath(token)
       };
     }
-  })
+  });
 
   return result || {
     token: 'package.json',
-    configurationExists: !!pkg.babel
-  };
-};
-
-const eslint = () => {
-  let result = null;
-
-  [
-    '.eslintrc',
-    '.eslintrc.json',
-    '.eslintrc.yml'
-  ].forEach(token => {
-    if (projectHasFile(token)) {
-      result = {
-        token,
-        configurationExists: true,
-        path: buildPath(token)
-      };
-    }
-  })
-
-  return result || {
-    token: 'package.json',
-    configurationExists: !!pkg.eslintConfig
+    configurationExists: !!pkg[pkgKey]
   };
 }
 
+const babel = () => fetch([
+  '.babelrc',
+  '.babelrc.js',
+  '.babel.config.js'
+], 'babel');
+
+const eslint = () => fetch([
+  '.eslintrc',
+  '.eslintrc.json',
+  '.eslintrc.yml',
+  '.eslintrc.yaml'
+], 'eslintConfig');
+
+const prettier = () => fetch([
+  '.prettierrc',
+  '.prettierrc.json',
+  '.prettierrc.yml',
+  '.prettierrc.yaml',
+  '.prettierrc.toml',
+  '.prettierrc.js',
+  'prettier.config.js',
+], 'prettier');
+
 module.exports = {
   babel,
-  eslint
+  eslint,
+  prettier
 };
