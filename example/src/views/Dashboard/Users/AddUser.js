87229e1c126c6272ee2b6adedb09543d0ea16876
Brian Maule <12001504+mauleb@users.noreply.github.com>
2019-10-23 13:14:42 -0500

    initial eject work

---
+++ example/src/views/Dashboard/Users/AddUser.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { Form } from '@jbknowledge/react-form';
+
+import { Input, Button } from 'src/common';
+
+const AddUser = ({ addUserAsync }) => (
+  <Form
+    onSubmit={({ formValid, values: user, resetInputs }) => {
+      if (formValid) {
+        addUserAsync(user);
+        resetInputs();
+      }
+    }}
+  >
+    <Input
+      name="firstName"
+      regex="^(?!\s*$).+"
+      defaultErrorMessage="First name required"
+      label="First Name"
+    />
+    <Input
+      name="lastName"
+      regex="^(?!\s*$).+"
+      defaultErrorMessage="Last name required"
+      label="Last Name"
+    />
+    <Button type="submit">Add User</Button>
+  </Form>
+);
+
+const mapDispatch = ({ users: { addUserAsync } }) => ({ addUserAsync });
+
+AddUser.propTypes = { addUserAsync: PropTypes.func };
+
+export default connect(
+  null,
+  mapDispatch
+)(AddUser);
